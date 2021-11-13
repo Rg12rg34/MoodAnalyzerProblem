12bb13a663dccd09503451ca20b5f2f930b94e07
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-13 08:17:20 +0530

    Wellcome to Mood Analyzer Problem

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+/**
+ * Purpose  - I am in Sad Mood” message Should Return SAD
+ * @author  - Rahul Gupta
+ * @version - 16.0
+ * @since   - 2021-11-09
+ */
+
+public class MoodAnalyser {
+    public static String analyseMood(String message) {
+        if (message.toLowerCase().contains("sad")) {
+        }
+        return "Sad";
+    }
+}
