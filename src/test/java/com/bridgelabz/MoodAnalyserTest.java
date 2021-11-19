f268dbbca885103bcee6a62376a85751fa88eb9f
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-19 20:00:39 +0530

    I am in Happy Mood' message in Constructor Should Return SAD

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -7,10 +7,16 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class MoodAnalyserTest
-{ @Test
+{   @Test
 public void givenMessage_WhenProper_ShouldReturnSad() {
     MoodAnalyser moodanalyser = new MoodAnalyser("I am in Sad Mood");
     String actualResult = moodanalyser.analyseMood();
     Assert.assertEquals("Sad", actualResult);
 }
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnHappy() {
+        MoodAnalyser moodanalyser = new MoodAnalyser("I am in happy Mood");
+        String actualResult = moodanalyser.analyseMood();
+        Assert.assertEquals("Happy", actualResult);
+    }
 }
